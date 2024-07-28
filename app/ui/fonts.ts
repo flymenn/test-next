9b8f1321f805b682df9cd976def7fc7dc8e9e02d
funingning <funingning@xin.com>
2024-07-28 13:44:41 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Literata, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const  lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
